a31be4572a91a7b491073d6e61184dfe181184c1
Monica Cruz <mcruzr9@hotmail.com>
2022-02-04 10:58:38 -0600

    Fin sockets

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+//app de express 
+const app = express();
+
+//node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+  
+
+//carpeta publica
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err)=> {
+
+    if (err) throw new Error(err);
+
+    console.log('Servidor corriendo e puerto',process.env.PORT);
+});
